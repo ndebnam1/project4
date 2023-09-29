bd10b1fe21a115c76a7ecf2c6be675e7f6e70b06
GoodGuyDoc <christiantay0113@gmail.com>
2023-09-29 08:43:46 -0400

    compiled these so branches would work

---
+++ compiled/p2.bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./p2.jsx":
+/*!****************!*\
+  !*** ./p2.jsx ***!
+  \****************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: C:\\Users\\Christian\\WebstormProjects\\project4\\p2.jsx: Support for the experimental syntax 'jsx' isn't currently enabled (7:3):\n\n\u001b[0m \u001b[90m  5 |\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m  6 |\u001b[39m \u001b[33mReactDOM\u001b[39m\u001b[33m.\u001b[39mrender(\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m  7 |\u001b[39m   \u001b[33m<\u001b[39m\u001b[33mStates\u001b[39m \u001b[33m/\u001b[39m\u001b[33m>\u001b[39m\u001b[33m,\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m    |\u001b[39m   \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m  8 |\u001b[39m   document\u001b[33m.\u001b[39mgetElementById(\u001b[32m'reactapp'\u001b[39m)\u001b[33m,\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m  9 |\u001b[39m )\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 10 |\u001b[39m\u001b[0m\n\nAdd @babel/preset-react (https://git.io/JfeDR) to the 'presets' section of your Babel config to enable transformation.\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://git.io/vb4yA) to the 'plugins' section to enable parsing.\n    at instantiate (C:\\Users\\Christian\\WebstormProjects\\project4\\node_modules\\@babel\\parser\\lib\\index.js:72:32)\n    at constructor (C:\\Users\\Christian\\WebstormProjects\\project4\\node_modules\\@babel\\parser\\lib\\index.js:358:12)\n    at Parser.raise (C:\\Users\\Christian\\WebstormProjects\\project4\\node_modules\\@babel\\parser\\lib\\index.js:3335:19)\n    at Parser.expectOnePlugin (C:\\Users\\Christian\\WebstormProjects\\project4\\node_modules\\@babel\\parser\\lib\\index.js:3392:18)\n    at Parser.parseExprAtom (C:\\Users\\Christian\\WebstormProjects\\project4\\node_modules\\@babel\\parser\\lib\\index.js:12970:18)\n    at Parser.parseExprSubscripts (C:\\Users\\Christian\\WebstormProjects\\project4\\node_modules\\@babel\\parser\\lib\\index.js:12540:23)\n    at Parser.parseUpdate (C:\\Users\\Christian\\WebstormProjects\\project4\\node_modules\\@babel\\parser\\lib\\index.js:12519:21)\n    at Parser.parseMaybeUnary (C:\\Users\\Christian\\WebstormProjects\\project4\\node_modules\\@babel\\parser\\lib\\index.js:12490:23)\n    at Parser.parseMaybeUnaryOrPrivate (C:\\Users\\Christian\\WebstormProjects\\project4\\node_modules\\@babel\\parser\\lib\\index.js:12284:61)\n    at Parser.parseExprOps (C:\\Users\\Christian\\WebstormProjects\\project4\\node_modules\\@babel\\parser\\lib\\index.js:12291:23)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./p2.jsx"]();
+/******/ 	
+/******/ })()
+;
